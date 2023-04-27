8fc4034050ca53ad39f423d6913381a195737982
Frank Collins <fwc3rd@gmail.com>
2023-04-27 19:57:12 -0400

    longjmp [7:57pm]
    https://stackoverflow.com/questions/3517726/what-is-wrong-with-using-goto

--- hikingtrail.c
+++ hikingtrail.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <setjmp.h>
 
 #define MAX_WORDS 100
 #define MAX_WORD_LENGTH 100
@@ -10,50 +11,43 @@ struct sentence {
     int length;
     char word[MAX_WORD_LENGTH];
 };
-    
-    char sen[MAX_WORD_LENGTH]; 
-    char portal;
-    int teleport = 0;
-    char sentence1[20];
-    char sentence2[20];
-    char sentence3[20];
 
-// void print(char *s, int newtab, int newline) 
-// print(char s) wont work it decays into a pointer. arg must be: *s || s[]
+char sen[MAX_WORD_LENGTH];
+char portal;
+int teleport = 0;
+char sentence1[20];
+char sentence2[20];
+char sentence3[20];
+jmp_buf buf;
+
 void print(char s[], int newtab, int newline) 
 {
     printf("my sentence:\t %s %c %s", s, newtab ? '\t' : '\0', newline ? "\n" : "\0");
-    // print("sure!", 1, 1); invoke like so!
 }
 
 int main() {
-    struct sentence my_sen[MAX_WORDS];    // initialize the struct
-    
-    
-    
-    print("hey thats a nice one!", 1, 1);
-    
+    struct sentence my_sen[MAX_WORDS];
+
+    print("hey that's a nice one!", 1, 1);
+
     scanf(" %[^\n]", &portal);
-    
-    // portal == 'o' ? goto word1 :  print("nice one" 1, 1); 
-    
+
     if (portal == 'o') {
         print("yeah", 1, 1);
-        
+        if (setjmp(buf) == 0) {
+            longjmp(buf, 1);
+        }
     }
-    
-    if (portal) {
-    word1: 
-    for (int i = 0; i < sizeof(sentence1); i++) { sentence1[i] = "\0"; }
-    scanf(" %[^\n]", sentence1);
-    // print("my_sentence1:\t %s", sentence1);
-    print(sentence1, 1, 1);
-    }
-    word_1_junior:
-    print("hehe", 1, 1);
-    
-    
 
-    
+    if (setjmp(buf) == 0) {
+        if (teleport == 1) {
+            printf("we're in here\n");
+            for (int i = 0; i < sizeof(sentence1); i++) { 
+                sentence1[i] = "\0"; 
+            }
+            scanf(" %[^\n]", sentence1);
+            print(sentence1, 1, 1);
+        }
+    }
     return 0;
 }
